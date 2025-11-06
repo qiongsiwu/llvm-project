0389ee5d544c0fab4b6914edbec521b587855458
Qiongsi Wu <qiongsi_wu@apple.com>
2025-11-06 14:06:42 -0800

    Fix incorrect stable dir initialization.

--- clang/lib/Tooling/DependencyScanning/DependencyScannerImpl.cpp
+++ clang/lib/Tooling/DependencyScanning/DependencyScannerImpl.cpp
@@ -1018,7 +1018,7 @@ bool CompilerInstanceWithContext::initialize(DiagnosticConsumer *DC) {
           Worker.Service, Worker.DepFS, false, nullptr, Worker.DepCASFS))
     return false;
 
-  llvm::SmallVector<StringRef> StableDirs = getInitialStableDirs(CI);
+  StableDirs = getInitialStableDirs(CI);
   auto MaybePrebuiltModulesASTMap =
       computePrebuiltModulesASTMap(CI, StableDirs);
   if (!MaybePrebuiltModulesASTMap)
