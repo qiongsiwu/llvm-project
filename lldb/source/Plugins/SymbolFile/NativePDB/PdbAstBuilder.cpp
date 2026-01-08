bb9a9a0acae0e7cbbf28bf48ea4f99a1fbcaad23
Leonard Grey <leonard@leonardgrey.com>
2026-01-08 14:44:31 -0500

    [LLDB][PDB] NFC: Change ToCompilerDecl(Context) signature (#175052)
    Follow-up to https://github.com/llvm/llvm-project/pull/173111
    
    `ToCompilerDeclContext` and `ToCompilerDecl` take their arguments as
    references, but all callers are pointers. This changes the signature to
    take pointers, then addresses the comments at
    https://github.com/llvm/llvm-project/pull/173111#discussion_r2673140167
    
    (cherry picked from commit 14af55c53acf12edfbe1f79d15d570660aafcecc)

--- lldb/source/Plugins/SymbolFile/NativePDB/PdbAstBuilder.cpp
+++ lldb/source/Plugins/SymbolFile/NativePDB/PdbAstBuilder.cpp
@@ -163,7 +163,7 @@ static bool IsAnonymousNamespaceName(llvm::StringRef name) {
 PdbAstBuilder::PdbAstBuilder(TypeSystemClang &clang) : m_clang(clang) {}
 
 lldb_private::CompilerDeclContext PdbAstBuilder::GetTranslationUnitDecl() {
-  return ToCompilerDeclContext(*m_clang.GetTranslationUnitDecl());
+  return ToCompilerDeclContext(m_clang.GetTranslationUnitDecl());
 }
 
 std::pair<clang::DeclContext *, std::string>
@@ -277,7 +277,7 @@ clang::Decl *PdbAstBuilder::GetOrCreateSymbolForId(PdbCompilandSymId id) {
 std::optional<CompilerDecl>
 PdbAstBuilder::GetOrCreateDeclForUid(PdbSymUid uid) {
   if (clang::Decl *result = TryGetDecl(uid))
-    return ToCompilerDecl(*result);
+    return ToCompilerDecl(result);
 
   clang::Decl *result = nullptr;
   switch (uid.kind()) {
@@ -301,7 +301,7 @@ PdbAstBuilder::GetOrCreateDeclForUid(PdbSymUid uid) {
   if (!result)
     return std::nullopt;
   m_uid_to_decl[toOpaqueUid(uid)] = result;
-  return ToCompilerDecl(*result);
+  return ToCompilerDecl(result);
 }
 
 clang::DeclContext *
@@ -321,7 +321,7 @@ PdbAstBuilder::GetOrCreateClangDeclContextForUid(PdbSymUid uid) {
 }
 
 CompilerDeclContext PdbAstBuilder::GetOrCreateDeclContextForUid(PdbSymUid uid) {
-  return m_clang.CreateDeclContext(GetOrCreateClangDeclContextForUid(uid));
+  return ToCompilerDeclContext(GetOrCreateClangDeclContextForUid(uid));
 }
 
 std::pair<clang::DeclContext *, std::string>
@@ -425,7 +425,7 @@ clang::DeclContext *PdbAstBuilder::GetParentClangDeclContext(PdbSymUid uid) {
 }
 
 CompilerDeclContext PdbAstBuilder::GetParentDeclContext(PdbSymUid uid) {
-  return m_clang.CreateDeclContext(GetParentClangDeclContext(uid));
+  return ToCompilerDeclContext(GetParentClangDeclContext(uid));
 }
 
 bool PdbAstBuilder::CompleteType(CompilerType ct) {
@@ -750,7 +750,7 @@ PdbAstBuilder::GetOrCreateTypedefDecl(PdbGlobalSymId id) {
   std::string uname = std::string(DropNameScope(udt.Name));
 
   CompilerType ct = ToCompilerType(qt).CreateTypedef(
-      uname.c_str(), ToCompilerDeclContext(*scope), 0);
+      uname.c_str(), ToCompilerDeclContext(scope), 0);
   clang::TypedefNameDecl *tnd = m_clang.GetAsTypedefDecl(ct);
   DeclStatus status;
   status.resolved = true;
@@ -1451,8 +1451,8 @@ void PdbAstBuilder::ParseDeclsForContext(CompilerDeclContext context) {
   }
 }
 
-CompilerDecl PdbAstBuilder::ToCompilerDecl(clang::Decl &decl) {
-  return m_clang.GetCompilerDecl(&decl);
+CompilerDecl PdbAstBuilder::ToCompilerDecl(clang::Decl *decl) {
+  return m_clang.GetCompilerDecl(decl);
 }
 
 CompilerType PdbAstBuilder::ToCompilerType(clang::QualType qt) {
@@ -1464,8 +1464,8 @@ clang::QualType PdbAstBuilder::FromCompilerType(CompilerType ct) {
 }
 
 CompilerDeclContext
-PdbAstBuilder::ToCompilerDeclContext(clang::DeclContext &context) {
-  return m_clang.CreateDeclContext(&context);
+PdbAstBuilder::ToCompilerDeclContext(clang::DeclContext *context) {
+  return m_clang.CreateDeclContext(context);
 }
 
 clang::Decl * PdbAstBuilder::FromCompilerDecl(CompilerDecl decl) {
