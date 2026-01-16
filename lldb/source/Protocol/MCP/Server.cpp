f4fb8a3deef7ac219577965be275011fca9a4c5a
Michael Buch <michaelbuch12@gmail.com>
2026-01-16 17:22:41 +0000

    [lldb][NFC] Replace createStringError/llvm::formatv pairs with createStringErrorV (#176428)
    Makes use of the recently introduced `llvm::createStringErrorV` API.
    It's designed exactly for this use-case.
    
    (cherry picked from commit f821e4b86a429d034e1eb057d93f8bfaf5708cda)

--- lldb/source/Protocol/MCP/Server.cpp
+++ lldb/source/Protocol/MCP/Server.cpp
@@ -162,7 +162,7 @@ llvm::Expected<Response> Server::ToolsCallHandler(const Request &request) {
 
   auto it = m_tools.find(tool_name);
   if (it == m_tools.end())
-    return llvm::createStringError(llvm::formatv("no tool \"{0}\"", tool_name));
+    return llvm::createStringErrorV("no tool \"{0}\"", tool_name);
 
   ToolArguments tool_args;
   if (const json::Value *args = param_obj->get("arguments"))
