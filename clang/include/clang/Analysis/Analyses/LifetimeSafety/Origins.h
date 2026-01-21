f21367d9cea580220df3635012b3c1c67d1fdc59
Gabor Horvath <gaborh@apple.com>
2026-01-21 17:40:47 +0000

    Fix compilation failure due to renamed header.

--- clang/include/clang/Analysis/Analyses/LifetimeSafety/Origins.h
+++ clang/include/clang/Analysis/Analyses/LifetimeSafety/Origins.h
@@ -16,7 +16,7 @@
 
 #include "clang/AST/Decl.h"
 #include "clang/AST/Expr.h"
-#include "clang/AST/TypeBase.h"
+#include "clang/AST/Type.h"
 #include "clang/Analysis/Analyses/LifetimeSafety/LifetimeStats.h"
 #include "clang/Analysis/Analyses/LifetimeSafety/Utils.h"
 #include "llvm/Support/raw_ostream.h"
