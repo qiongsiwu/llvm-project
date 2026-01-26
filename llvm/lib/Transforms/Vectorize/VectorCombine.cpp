d2e3f44dcd7b8ef973d9c14d3cea8684f1b58656
Valeriy Savchenko <vsavchenko@apple.com>
2026-01-26 17:48:10 +0000

    [VectorCombine] Call cost calculation with correct intrinsic IDs (#177996)
    #175194, #177159, and #173069 introduced the code calling
    `TTI.getMinMaxReductionCost` with unexpected `Intrinsic::ID` causing
    RISC-V to fail with `llvm_unreachable` panic.
    
    Functionally, this is a small fix that also ports tests for the
    aforementioned folds to RISCV.

--- llvm/lib/Transforms/Vectorize/VectorCombine.cpp
+++ llvm/lib/Transforms/Vectorize/VectorCombine.cpp
@@ -3412,7 +3412,8 @@ bool VectorCombine::foldSignBitReductionCmp(Instruction &I) {
         TTI.getArithmeticReductionCost(getArithmeticReductionInstruction(Arith),
                                        VecTy, std::nullopt, CostKind);
     InstructionCost MinMaxCost =
-        TTI.getMinMaxReductionCost(MinMax, VecTy, FastMathFlags(), CostKind);
+        TTI.getMinMaxReductionCost(getMinMaxReductionIntrinsicOp(MinMax), VecTy,
+                                   FastMathFlags(), CostKind);
     return ArithCost <= MinMaxCost ? std::make_pair(Arith, ArithCost)
                                    : std::make_pair(MinMax, MinMaxCost);
   };
@@ -3586,10 +3587,10 @@ bool VectorCombine::foldICmpEqZeroVectorReduce(Instruction &I) {
     case Intrinsic::vector_reduce_umax:
     case Intrinsic::vector_reduce_smin:
     case Intrinsic::vector_reduce_smax:
-      OldReduceCost =
-          TTI.getMinMaxReductionCost(IID, FXTy, FastMathFlags(), CostKind);
-      NewReduceCost =
-          TTI.getMinMaxReductionCost(IID, XTy, FastMathFlags(), CostKind);
+      OldReduceCost = TTI.getMinMaxReductionCost(
+          getMinMaxReductionIntrinsicOp(IID), FXTy, FastMathFlags(), CostKind);
+      NewReduceCost = TTI.getMinMaxReductionCost(
+          getMinMaxReductionIntrinsicOp(IID), XTy, FastMathFlags(), CostKind);
       break;
     default:
       llvm_unreachable("Unexpected reduction");
@@ -3732,7 +3733,8 @@ bool VectorCombine::foldEquivalentReductionCmp(Instruction &I) {
     if (ReductionOpc != Instruction::ICmp)
       return TTI.getArithmeticReductionCost(ReductionOpc, VecTy, std::nullopt,
                                             CostKind);
-    return TTI.getMinMaxReductionCost(IID, VecTy, FastMathFlags(), CostKind);
+    return TTI.getMinMaxReductionCost(getMinMaxReductionIntrinsicOp(IID), VecTy,
+                                      FastMathFlags(), CostKind);
   };
 
   InstructionCost OrigCost = GetReductionCost(OriginalIID);
